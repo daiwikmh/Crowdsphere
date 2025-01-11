822b3cc490ae49d39bf40d1d3e88deab7c74db64
daiwikmh <daiwikdomain@gmail.com>
2025-01-11 21:28:53 +0530

    websiteworkinsstartanddependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import React from "react";
 
 
-const App: React.FC = () => {
-  return (
+export function App() {
+	return (
+		<div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+			<div className="sm:flex hidden mr-10 relative">
+				Sidebar
+			</div>
+			<div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto am:pr-5">
+				Navbar
+			</div>
+		</div>
+	);
+}
 
-  );
-};
 
-export default App;
+
+
 
